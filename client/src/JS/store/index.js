d02f479628d507272b8728bb6d679d8899a763e2
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 20:30:02 +0100

    implement the redux store

---
+++ client/src/JS/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import combineReducer from '../reducers/index';
+const middleWare = [thunk];
+
+const composeEnhancers =
+  window.__REDUX_DEV_TOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {};
+
+const Store = createStore(
+  combineReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
+
+export default Store;
