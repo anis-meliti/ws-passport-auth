cb5320afcf8dd60e91e811c12e6936868b903eaa
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 20:46:30 +0100

    implement the register action

--- client/src/JS/actions/actions.js
+++ client/src/JS/actions/actions.js
@@ -1,10 +1,24 @@
-const initialState = {};
+import axios from 'axios';
+import {
+  REGISTER_USER,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS
+} from '../constants/actions-types';
 
-const authReducer = (state = initialState, { action, payload }) => {
-  switch (action) {
-    default:
-      return state;
+export const register = user => async dispatch => {
+  dispatch({
+    type: REGISTER_USER
+  });
+  try {
+    const registerResult = await axios.post('/register', user);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: registerResult.data
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_FAIL,
+      payload: error.response.data.errors
+    });
   }
 };
-
-export default authReducer;
