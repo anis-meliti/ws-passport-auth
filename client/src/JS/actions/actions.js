54760d2bcfabb1ab004a58f6aec68632b429b00e
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 20:36:36 +0100

    impelement the authReducer

---
+++ client/src/JS/actions/actions.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const authReducer = (state = initialState, { action, payload }) => {
+  switch (action) {
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
