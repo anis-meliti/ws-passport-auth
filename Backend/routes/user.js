1af5009754067bd0f243b1d0c0bda346466517f5
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 13:41:10 +0100

    implement the route of current user

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -7,6 +7,6 @@ const Router = express.Router();
 
 Router.post('/register', registerRules(), validator, userController.register);
 Router.post('/login', userController.login);
-Router.post('/current', isAuth(), (req, res) => res.json({ user: req.user }));
+Router.get('/current', isAuth(), (req, res) => res.json({ user: req.user }));
 
 module.exports = Router;
