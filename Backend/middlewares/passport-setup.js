365626421cca6cfc91555b30de2e1fadb1388bbf
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 13:34:13 +0100

    implement the passport authentication middleware

---
+++ Backend/middlewares/passport-setup.js
@@ -0,0 +1,29 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const config = require('config');
+const passport = require('passport');
+
+const User = require('../models/User');
+const secretOrKey = config.get('secretOrKey');
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey
+};
+
+passport.initialize();
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const searchResult = await User.findById(jwt_payload.id).select(
+        '-password'
+      );
+      searchResult ? done(null, searchResult) : done(null, false);
+    } catch (error) {
+      console.error(error);
+    }
+  })
+);
+module.exports = isAuth = () =>
+  passport.authenticate('jwt', { session: false });
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const userController = require('../controllers/user.Controller');
 const { registerRules, validator } = require('../middlewares/validator');
+const isAuth = require('../middlewares/passport-setup');
 const Router = express.Router();
 
 Router.post('/register', registerRules(), validator, userController.register);
 Router.post('/login', userController.login);
+Router.post('/current', isAuth(), (req, res) => res.json({ user: req.user }));
 
 module.exports = Router;
