c3871b16f72ccc32304b248ed8adb19741a4db8f
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 21:05:02 +0100

    structure the front route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import './App.css';
 
+import SignIn from './Components/SignIn';
+import SignUp from './Components/SignUp';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Route exact path='/' render={() => <SignIn />} />
+        <Route exact path='/register' render={() => <SignUp />} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
