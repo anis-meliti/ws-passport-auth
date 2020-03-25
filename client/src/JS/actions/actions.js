91c1c8b8bf7d09c82d29fbd59da4e9a58819ea07
Anis Melliti <ansmeliti@gmail.com>
2020-03-25 22:17:32 +0100

    implement the login action

--- client/src/JS/actions/actions.js
+++ client/src/JS/actions/actions.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 import {
   REGISTER_USER,
   REGISTER_FAIL,
-  REGISTER_SUCCESS
+  REGISTER_SUCCESS,
+  LOGIN_USER,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS
 } from '../constants/actions-types';
 
 export const register = user => async dispatch => {
@@ -18,6 +21,24 @@ export const register = user => async dispatch => {
   } catch (error) {
     dispatch({
       type: REGISTER_FAIL,
+      payload: error.response.data.errors
+    });
+  }
+};
+
+export const login = userCredential => async dispatch => {
+  dispatch({
+    type: LOGIN_USER
+  });
+  try {
+    const loginResult = await axios.post('/login', userCredential);
+    localStorage.setItem('token', loginResult.data.token);
+    dispatch({
+      type: LOGIN_SUCCESS
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGIN_FAIL,
       payload: error.response.data.errors
     });
   }
