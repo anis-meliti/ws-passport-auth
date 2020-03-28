f91216fc933285628a5fec7e1907e56331a16194
Anis Melliti <ansmeliti@gmail.com>
2020-03-28 11:54:27 +0100

    make it ready for deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const connectDb = require('./config/connectDb');
 const user = require('./routes/user');
 const app = express();
@@ -8,6 +8,13 @@ app.use(express.json());
 connectDb();
 
 app.use('/', user);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname, 'client', 'build', 'index.html');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
